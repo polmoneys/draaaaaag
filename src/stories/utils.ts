cc95cc6f2931bbad87f70d1c0163c6c797459268
“Pol <p0lm0n3ys@gmail.com>
2023-04-14 07:51:04 +0200

    docs : update demo

--- src/stories/utils.ts
+++ src/stories/utils.ts
@@ -1,5 +1,14 @@
 import { Card } from '@/lib/types'
 
+interface InertElement extends HTMLElement {
+  inert: boolean
+}
+
+export const inertMain = (status = true): void => {
+  const main = document?.querySelector('main')
+  if (main != null) (main as InertElement).inert = status
+}
+
 export const cards: Card[] = [...Array(16)].map((_, pos) => ({
   id: `card-${pos}`,
   content: `Card ${pos}`,
